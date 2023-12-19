c1f2c8c064e73b9ce1c59e0fecbb4506bfc221ab
tahseenadit <tahseen.adit@gmail.com>
2023-12-19 17:57:04 +0000

    added resources

--- learn-python/list_initialization.py
+++ learn-python/list_initialization.py
@@ -27,4 +27,18 @@ my_list2 = [[] for _ in range(num_of_items)]
 for item in range(0, num_of_items):
     my_list2[item] = 2
 
+"""
+my_list Output:
+my_list: [[1, 2, 2, 2, 2, 2], [1, 2, 2, 2, 2, 2], [1, 2, 2, 2, 2, 2], [1, 2, 2, 2, 2, 2], [1, 2, 2, 2, 2, 2]]
+Explanation: In my_list, each element is a reference to the same list. Therefore, when you append 2 in the loop, it appends 2 five times to this single shared list. 
+That's why you see each element of my_list reflecting the same list with [1] followed by five 2s.
+
+my_list2 Output:
+my_list2: [[2], [2], [2], [2], [2]]
+Explanation: In my_list2, each element is an independent list. So, when you append 2 in the loop, it appends 2 once to each separate list. As a result, each element 
+in my_list2 is an individual list containing [2].
+This demonstrates how list initialization impacts the behavior of the lists, especially when modifying them in loops. my_list contains multiple references to the same 
+list, leading to collective changes, whereas my_list2 contains independent lists, leading to individual changes.
+"""
+
  
